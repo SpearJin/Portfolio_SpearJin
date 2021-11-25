14bfe784d448cf0583cab4e59f95ab90d8ef54c1
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-11-25 19:29:51 +0900

    feat: 스크롤 내리면 navbar 배경색 변화 구현

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const navbar = document.querySelector('.navbar');
 const navbarMenu = document.querySelector('.navbar__menu');
-
+const navbarMenuRect = navbar.getBoundingClientRect();
+const navbarHeight = navbarMenuRect.height;
 // 메뉴바 클릭시 해당 태그로 이동
 navbarMenu.addEventListener('click', (e) => {
   e.preventDefault();
@@ -9,4 +11,12 @@ navbarMenu.addEventListener('click', (e) => {
   const link = target.dataset.link;
   const chosen = document.querySelector(link);
   chosen.scrollIntoView({ behavior: 'smooth' });
+});
+
+window.addEventListener('scroll', (e) => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar-dark');
+  } else {
+    navbar.classList.remove('navbar-dark');
+  }
 });
