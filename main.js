a411490ecb90414c16efbc704c5193de47d42546
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-11-27 15:34:36 +0900

    feat: Arrow버튼 마크업&디자인&클릭시 home화면으로 이동 구현

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ const navbar = document.querySelector('.navbar');
 const navbarMenu = document.querySelector('.navbar__menu');
 const navbarMenuRect = navbar.getBoundingClientRect();
 const navbarHeight = navbarMenuRect.height;
+
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+
+const arrow = document.querySelector('.arrow');
 // 메뉴바 클릭시 해당 태그로 이동
 navbarMenu.addEventListener('click', (e) => {
   e.preventDefault();
   const target = e.target;
   const link = target.dataset.link;
   const chosen = document.querySelector(link);
-  chosen.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  scrollInto(chosen);
 });
 
 window.addEventListener('scroll', (e) => {
@@ -22,9 +27,21 @@ window.addEventListener('scroll', (e) => {
 });
 
 // 스크롤 내리면 홈 투명도 주기
-const home = document.querySelector('#home');
-const homeHeight = home.getBoundingClientRect().height;
-
 window.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
+  if (window.scrollY > homeHeight) {
+    arrow.classList.remove('hidden');
+  } else {
+    arrow.classList.add('hidden');
+  }
 });
+
+// Arrow 버튼을 누르면 Home화면으로 이동
+
+arrow.addEventListener('click', () => {
+  scrollInto(home);
+});
+
+function scrollInto(link) {
+  link.scrollIntoView({ behavior: 'smooth', block: 'center' });
+}
